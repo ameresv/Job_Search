d22b51d03b2cc1706af05d912be4f990019b225a
Alí Meres <47176573+ameresv@users.noreply.github.com>
2023-07-01 10:22:10 -0500

    Update test.sh - new key

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # rm ./otros.txt 
-# key="QPMGL8ZP1SHHEM900DRNVPIAF3M9XJX7GHO6UU5HAWZFFNLD26985GKDRT2F2NQ6AEEC6T2WMJG1XMJC"
+# key="D5NBVDLUWATIJR92ZJE3JCV3JNY1ZV3E4BBJNCCOMF2BZ695A2RFV7WM1PFT13IBKORQ86FRIYNPQAMY"
 # url="https://www.bumeran.com.pe/perfiles/empresa_hudbay-peru_10097690.html"
 # curl -s "https://app.scrapingbee.com/api/v1/?api_key=${key}&url=${url}" > otros.txt
 
